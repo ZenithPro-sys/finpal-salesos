b1f9e6ba4dd6054f11ab7980971ee8dca24943e3
ZenithPro-sys <zeenatranderee89@gmail.com>
2026-06-17 09:11:29 +0200

    fix: clean next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   },
+  typescript: {
+    // We handle type checking separately
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
